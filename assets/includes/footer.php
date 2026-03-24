ea72e6657b8d57a64b18a5686f55783c0b83699f
Klara Klingberg <klara.klingberg0001@stud.hkr.se>
2026-03-24 14:18:28 +0000

    Startsida fix med bild, footertext

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,9 +1,13 @@
-<footer class="container text-muted d-flex justify-content-between mt-4">
-    <p class="text-muted">FOOTER TEXT HÄR </p>
-    <p>
+<footer class="container d-flex justify-content-between py-4 border-top">
+    <p class="text-muted">
+        <i class="fa-regular fa-copyright"></i> 2026 SkillSwap - En plattform skapad av studenter,
+        för studenter. Dela din kunskap och lär dig något nytt idag.
+    </p>
+
+    <div class="d-flex gap-3">
         <i class="fab fa-facebook icon-facebook"></i>
         <i class="fab fa-twitter icon-twitter"></i>
         <i class="fab fa-instagram icon-instagram"></i>
         <i class="fab fa-flickr icon-flickr"></i>
-    </p>
+    </div>
 </footer>
--- index.php
+++ index.php
@@ -55,53 +55,33 @@ Du har lyckats logga ut! :-)
 
 <body>
     <!--Taggar, header osv-->
-    <main class="background vh-100 d-flex align-items-center text-white">
+    <main class="background d-flex flex-column text-white">
+
         <div class="container text-center">
             <h1 class="display-1 fw-bold mb-4 mt-5">Skillswap</h1>
             <p class="lead fs-4 mb-4">En plattform där studenter kan dela sina kunskaper med varandra</p>
-            <div class="mb-4 text-center">
-                <a href="add.php" class="btn btn-primary btn-lg me-2">Skapa konto</a>
-                <a href="login.php" class="btn btn-success btn-lg">Logga in</a>
-            </div>
-            <!--LÄGGA IN BILD HÄR-->
+           
             <div class="mb-4">
-                <img src="assets/images/" alt="Skillswap">
+                <img src="assets/images/skillswap.webp" alt="Skillswap" style="max-width: 500px;">
             </div>
+
             <div class="mb-4">
                 <h2>Hur fungerar det?</h2>
                 <p>Skapa en profil, skapa ett inlägg, hjälp andra</p>
             </div>
-            <h2>Taggar</h2>
-            <!--försök till taggar kopplat till tag.php-->
-            <ul class="list-unstyled d-flex gap-2 flex-wrap">
-                <li>
-                    <a href="tag.php?programmering" class="badge bg-primary">#Programmering</a>
-                </li>
-
-                <li>
-                    <a href="tag.php?matematik" class="badge bg-primary">#Matematik</a>
-
-                </li>
-
-                <li>
-                    <a href="tag.php?design" class="badge bg-primary">#Design</a>
-                </li>
-
-                <li>
-                    <a href="tag.php?ux" class="badge bg-primary">#UX</a>
-                </li>
-                <li>
-                    <a href="tag.php?svenska" class="badge bg-primary">#Svenska</a>
-                </li>
-            </ul>
+            
+            <div class="mb-4 ">
+                <a href="add.php" class="btn btn-primary btn-lg me-2">Skapa konto</a>
+                <a href="login.php" class="btn btn-success btn-lg">Logga in</a>
+            </div>
         </div>
     </main>
 
-
+    <?php
+    //include footer
+    require_once 'assets/includes/footer.php';
+    ?>
 </body>
-<?php
-//include footer
-require_once 'assets/includes/footer.php';
-?>
+
 
 </html>
